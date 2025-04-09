0432d40f621fbc48a5bf10e956618796ae5545b9
rabiiinnn <rabinadhikari148@gmail.com>
2025-04-09 21:32:06 +0545

    student made

--- school_management_system/Student_Views.py
+++ school_management_system/Student_Views.py
@@ -1,1 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect,HttpResponse
+
+def student_register(request):
+    if request.method == 'POST':
+        # handle form submission here
+        # e.g., save to DB
+        return HttpResponse("Student registered successfully!")
+    return render(request, 'student_register.html')  # or whatever your template is
+def student(request):
+    return render(request,'student.html')
